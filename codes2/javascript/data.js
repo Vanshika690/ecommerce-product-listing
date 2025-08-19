64109dc01f7226d2b14e834f9b9316c9467870c3
Vanshika690 <guptavanshika22492@gmail.com>
2025-08-19 19:18:56 +0530

    Add files via upload

---
+++ codes2/javascript/data.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: "Smartphone",
+    price: 299,
+    category: "electronics",
+    image: "images/phone.jpg"
+  },
+  {
+    id: 2,
+    name: "Laptop",
+    price: 899,
+    category: "electronics",
+    image: "images/laptop.jpg"
+  },
+  {
+    id: 3,
+    name: "T-Shirt",
+    price: 19,
+    category: "clothing",
+    image: "images/tshirt.jpg"
+  },
+  {
+    id: 4,
+    name: "Sneakers",
+    price: 79,
+    category: "clothing",
+    image: "images/sneakers.jpg"
+  }
+];
